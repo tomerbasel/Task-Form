5b6991c1c4e94c73e28e216150750a54bcb8e4e0
tomer basel <tbasel@gmail.com>
2014-08-30 16:53:58 +0300

    working version 80%

--- myscript.js
+++ myscript.js
@@ -49,3 +49,20 @@ function formsummery(){
 	
 }
 
+function validation(){
+
+	var name= document.forms["theForm"]["name"].value;
+	if (name == null || name=="") {alert ('please chack for missing details');
+	return false
+	} 
+	var lastname= document.forms["theForm"]["lastName"].value;
+	if (lastname == null || lastname=="") {alert ('please chack for missing details');
+	return false;}
+
+	var x= document.forms["theForm"]["mail"].value;
+    var atpos = x.indexOf("@");
+    var dotpos = x.lastIndexOf(".");
+    if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
+        alert("Not a valid e-mail address");
+        return false;}
+}
